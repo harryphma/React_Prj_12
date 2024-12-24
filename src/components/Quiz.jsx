f68c8a6a832cbee515612778dfe7194580915b2d
harryphma <phamquanghung2705@gmail.com>
2024-12-23 21:29:04 -0700

    fix some bugs

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { act, useState } from "react";
+import { useState, useCallback } from "react";
 import QuestionTimer from "./QuestionTimer.jsx";
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png'
@@ -9,12 +9,13 @@ export default function Quiz(){
     
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    function handleSelectAnswer(selectedAnswer){
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
-    }
+    }, []);
 
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
     if (quizIsComplete){
         return (
             <div id="summary">
@@ -31,7 +32,7 @@ export default function Quiz(){
     return (
         <div id="quiz"> 
             <div id="question">
-                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}></QuestionTimer>
+                <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer}></QuestionTimer>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map(answer => (
